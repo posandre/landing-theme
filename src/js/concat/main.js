02ee51d61f43af5f4782b3c5c06c55e46f1d5c31
andriipostoliuk <andriy.postoluk@inoxoft.com>
2023-12-10 18:02:25 +0200

    added animated anchor scroll

--- src/js/concat/main.js
+++ src/js/concat/main.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function($) {
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
 
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
 });
 
 
