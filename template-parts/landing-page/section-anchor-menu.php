60f36f3bb3f6833086bac3ffdbc3939e3697960d
andriipostoliuk <andriy.postoluk@inoxoft.com>
2023-12-10 18:02:50 +0200

    corrected anchor menu template

--- template-parts/landing-page/section-anchor-menu.php
+++ template-parts/landing-page/section-anchor-menu.php
@@ -3,9 +3,9 @@ $anchor_navigation_items = landing_theme_get_acf_field('anchor_navigation_items'
 
 if (empty($anchor_navigation_items)) return;
 
-$output = '<div class="anchor-navigation">';
+$output = '<div class="anchor-navigation container">';
 $output .=  '<div class="anchor-navigation__title">' .__('ANCHOR NAV', 'landing-theme'). '</div>';
-$output .=  '<ul class="anchor-navigation__items"';
+$output .=  '<ul class="anchor-navigation__items">';
 
 foreach ($anchor_navigation_items as $anchor_navigation_item) {
 	$output .= '<li class="anchor-navigation__item">
