82341e614e10caa870bcdfeffff511a94db77741
andriipostoliuk <andriy.postoluk@inoxoft.com>
2023-12-11 13:01:45 +0200

    corrected 2-3 cards template

--- template-parts/landing-page/section-column-card-2-3.php
+++ template-parts/landing-page/section-column-card-2-3.php
@@ -1,23 +1,23 @@
 <?php
-$section_data = $args['data']['2_3_column_card'];
+$section_data = $args['data']['column_card_2_3'];
 
 if (empty($section_data)) return;
 ?>
 
-<div class="2-3-column-card__container container">
+<div class="column-card-2-3__container container">
     <?php if (!empty($section_data['title'])) : ?>
-        <div class="2-3-column-card__title"><h2><?php echo $section_data['title']; ?></h2></div>
+        <div class="column-card-2-3__title"><h2><?php echo $section_data['title']; ?></h2></div>
     <?php endif; ?>
 
 	<?php if (!empty($section_data['cards'])) : ?>
-    <div class="2-3-column-card__cards">
+    <div class="column-card-2-3__cards">
         <?php foreach ($section_data['cards'] as $card) : ?>
-        <div class="2-3-column-card__card">
+        <div class="column-card-2-3__card">
 	        <?php if (!empty($card['image'] && !in_array('hide_image', $card['hide_card_content']))) :
 		        $img_alt = empty($card['title'])
 			        ? __('2-3-column-card img', 'landing-theme')
 			        : $card['title']; ?>
-                <div class="2-3-column-card__card-image">
+                <div class="column-card-2-3__card-image">
 	                <?php  landing_theme_the_attachment_image(
 		                $card['image'],
 		                '2_3_column_cards_img',
@@ -29,20 +29,20 @@ if (empty($section_data)) return;
 	        <?php endif; ?>
 
 	        <?php if (!empty($card['title'] && !in_array('hide_title', $card['hide_card_content']))) :?>
-                <div class="2-3-column-card__card-title 2-3-column-card__card-title--<?php echo $card['title_style']; ?>">
+                <div class="column-card-2-3__card-title column-card-2-3__card-title--<?php echo $card['title_style']; ?>">
 			        <?php  echo $card['title'];?>
                 </div>
 	        <?php endif; ?>
 
 	        <?php if (!empty($card['description'] && !in_array('hide_description', $card['hide_card_content']))) :?>
-                <div class="2-3-column-card__card-description">
+                <div class="column-card-2-3__card-description">
 			        <?php echo apply_filters( 'the_content', $card['description']);?>
                 </div>
 	        <?php endif; ?>
 
 	        <?php if (!empty($card['link'] && !in_array('hide_link', $card['hide_card_content']))) :?>
-                <div class="2-3-column-card__card-link">
-                    <a class="" href="<?php echo $card['link']; ?>"><?php _e('Learn more', 'landing-theme'); ?></a>
+                <div class="column-card-2-3__card-link">
+                    <a href="<?php echo $card['link']; ?>"><?php _e('Learn more', 'landing-theme'); ?></a>
                 </div>
 	        <?php endif; ?>
         </div>
